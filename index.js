f436f422ab836528298cf3f4aae08af467dcff20
bgtrade <47718076+bgtrade@users.noreply.github.com>
2019-02-17 10:14:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 'use strict';
 
-var bgrade = module.exports;
+var bgtrade = module.exports;
 
 // module information
-bgrade.version = 'v' + require('./package.json').version;
-bgrade.versionGuard = function(version) {
+bgtrade.version = 'v' + require('./package.json').version;
+bgtrade.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bgrade-lib found. ' +
-      'Please make sure to require bgrade-lib and check that submodules do' +
-      ' not also include their own bgrade-lib dependency.';
+    var message = 'More than one instance of bgtrade-lib found. ' +
+      'Please make sure to require bgtrade-lib and check that submodules do' +
+      ' not also include their own bgtrade-lib dependency.';
     throw new Error(message);
   }
 };
-bgrade.versionGuard(global._bgrade);
-global._bgrade = bgrade.version;
+bgtrade.versionGuard(global._bgtrade);
+global._bgtrade = bgtrade.version;
 
 // crypto
-bgrade.crypto = {};
-bgrade.crypto.BN = require('./lib/crypto/bn');
-bgrade.crypto.ECDSA = require('./lib/crypto/ecdsa');
-bgrade.crypto.Hash = require('./lib/crypto/hash');
-bgrade.crypto.Random = require('./lib/crypto/random');
-bgrade.crypto.Point = require('./lib/crypto/point');
-bgrade.crypto.Signature = require('./lib/crypto/signature');
+bgtrade.crypto = {};
+bgtrade.crypto.BN = require('./lib/crypto/bn');
+bgtrade.crypto.ECDSA = require('./lib/crypto/ecdsa');
+bgtrade.crypto.Hash = require('./lib/crypto/hash');
+bgtrade.crypto.Random = require('./lib/crypto/random');
+bgtrade.crypto.Point = require('./lib/crypto/point');
+bgtrade.crypto.Signature = require('./lib/crypto/signature');
 
 // encoding
-bgrade.encoding = {};
-bgrade.encoding.Base58 = require('./lib/encoding/base58');
-bgrade.encoding.Base58Check = require('./lib/encoding/base58check');
-bgrade.encoding.BufferReader = require('./lib/encoding/bufferreader');
-bgrade.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
-bgrade.encoding.Varint = require('./lib/encoding/varint');
+bgtrade.encoding = {};
+bgtrade.encoding.Base58 = require('./lib/encoding/base58');
+bgtrade.encoding.Base58Check = require('./lib/encoding/base58check');
+bgtrade.encoding.BufferReader = require('./lib/encoding/bufferreader');
+bgtrade.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
+bgtrade.encoding.Varint = require('./lib/encoding/varint');
 
 // utilities
-bgrade.util = {};
-bgrade.util.buffer = require('./lib/util/buffer');
-bgrade.util.js = require('./lib/util/js');
-bgrade.util.preconditions = require('./lib/util/preconditions');
+bgtrade.util = {};
+bgtrade.util.buffer = require('./lib/util/buffer');
+bgtrade.util.js = require('./lib/util/js');
+bgtrade.util.preconditions = require('./lib/util/preconditions');
 
 // errors thrown by the library
-bgrade.errors = require('./lib/errors');
+bgtrade.errors = require('./lib/errors');
 
 // main bitcoin library
-bgrade.Address = require('./lib/address');
-bgrade.Block = require('./lib/block');
-bgrade.MerkleBlock = require('./lib/block/merkleblock');
-bgrade.BlockHeader = require('./lib/block/blockheader');
-bgrade.HDPrivateKey = require('./lib/hdprivatekey.js');
-bgrade.HDPublicKey = require('./lib/hdpublickey.js');
-bgrade.Networks = require('./lib/networks');
-bgrade.Opcode = require('./lib/opcode');
-bgrade.PrivateKey = require('./lib/privatekey');
-bgrade.PublicKey = require('./lib/publickey');
-bgrade.Script = require('./lib/script');
-bgrade.Transaction = require('./lib/transaction');
-bgrade.URI = require('./lib/uri');
-bgrade.Unit = require('./lib/unit');
+bgtrade.Address = require('./lib/address');
+bgtrade.Block = require('./lib/block');
+bgtrade.MerkleBlock = require('./lib/block/merkleblock');
+bgtrade.BlockHeader = require('./lib/block/blockheader');
+bgtrade.HDPrivateKey = require('./lib/hdprivatekey.js');
+bgtrade.HDPublicKey = require('./lib/hdpublickey.js');
+bgtrade.Networks = require('./lib/networks');
+bgtrade.Opcode = require('./lib/opcode');
+bgtrade.PrivateKey = require('./lib/privatekey');
+bgtrade.PublicKey = require('./lib/publickey');
+bgtrade.Script = require('./lib/script');
+bgtrade.Transaction = require('./lib/transaction');
+bgtrade.URI = require('./lib/uri');
+bgtrade.Unit = require('./lib/unit');
 
 // dependencies, subject to change
-bgrade.deps = {};
-bgrade.deps.bnjs = require('bn.js');
-bgrade.deps.bs58 = require('bs58');
-bgrade.deps.Buffer = Buffer;
-bgrade.deps.elliptic = require('elliptic');
-bgrade.deps._ = require('lodash');
+bgtrade.deps = {};
+bgtrade.deps.bnjs = require('bn.js');
+bgtrade.deps.bs58 = require('bs58');
+bgtrade.deps.Buffer = Buffer;
+bgtrade.deps.elliptic = require('elliptic');
+bgtrade.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
-bgrade.Transaction.sighash = require('./lib/transaction/sighash');
+bgtrade.Transaction.sighash = require('./lib/transaction/sighash');
